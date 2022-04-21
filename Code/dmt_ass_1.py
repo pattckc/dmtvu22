70f42754e255990e867702547088f92de396f468
pattckc <71180317+pattckc@users.noreply.github.com>
2022-04-22 00:33:16 +0200

    Update dmt_ass_1.py

--- Code/dmt_ass_1.py
+++ Code/dmt_ass_1.py
@@ -3,12 +3,16 @@ from tokenize import Ignore
 import pandas as pd
 import matplotlib.pyplot as plt
 from dateutil.parser import parse
-from pandas.errors import ParserError
 from dateutil.tz import gettz
-import time
 from time import strptime
 from datetime import datetime
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 tzinfos = gettz('Europe / Berlin')
 
 df = pd.read_csv('Code\Data\ODI-2022.csv', header = 0, sep = ';')
@@ -135,4 +139,100 @@ var4 = df.iloc[:, 4].value_counts(); var5 = df.iloc[:, 5].value_counts(); var6 =
 var8 = df.iloc[:, 8].value_counts(); var9 = df.iloc[:, 9].value_counts(); var10 = df.iloc[:, 10].value_counts(); var11 = df.iloc[:, 11].value_counts()
 var12 = df.iloc[:, 12].value_counts(); var13 = df.iloc[:, 13].value_counts(); var14 = df.iloc[:, 14].value_counts(); var15 = df.iloc[:, 15].value_counts(); 
 var16 = df.iloc[:, 16].value_counts()
-print(df)
+
+
+histprog = df.iloc[:, 1]
+histprog.hist(bins=5)
+plt.ylabel('Frequency')
+#plt.xlabel("Artificial Intelligence, Computer Science, Business Analytics, Computational Science, Other")
+plt.title('What programme are you in?')
+#plt.show()
+
+histml = df.iloc[:, 2]
+histml.hist(bins=3)
+plt.ylabel('Frequency')
+#plt.xlabel('No, Yes, Unknown')
+plt.title('Have you taken a course on machine learning?')
+#plt.show()
+
+histir = df.iloc[:, 3]
+histir.hist(bins=3)
+plt.ylabel('Frequency')
+#plt.xlabel('No = 0, Yes = 1, Unknown')
+plt.title('Have you taken a course on information retrieval')
+#plt.show()
+
+histst = df.iloc[:, 4]
+histst.hist(bins=3)
+plt.ylabel('Frequency')
+#plt.xlabel('No = Sigma, Yes = Mu , Unknown')
+plt.title('Have you taken a course on statistics')
+#plt.show()
+
+histdb = df.iloc[:, 5]
+histdb.hist(bins=3)
+plt.ylabel('Frequency')
+#plt.xlabel('No = Nee, Yes = Ja, Unknown')
+plt.title('Have you taken a course on databases?')
+#plt.show()
+
+histgen = df.iloc[:, 6]
+histgen.hist(bins=5)
+plt.ylabel('Frequency')
+#plt.xlabel('Male, Female, Unknown')
+plt.title('What is your gender?')
+#plt.show()
+
+#encoding all the data
+df['eattml'] = LabelEncoder().fit_transform(df.iloc[:, 2])
+df['eattir'] = LabelEncoder().fit_transform(df.iloc[:, 3])
+df['eattst'] = LabelEncoder().fit_transform(df.iloc[:, 4])
+df['eattdb'] = LabelEncoder().fit_transform(df.iloc[:, 5])
+df['eattgen'] = LabelEncoder().fit_transform(df.iloc[:, 6])
+df['etarget'] = LabelEncoder().fit_transform(df.iloc[:, 1])
+#creat input and output
+target = df['etarget']
+atts = df[['eattml', 'eattir' ,'eattst' ,'eattdb' ,'eattgen']]
+print(atts)
+print(target)
+
+# splitting data into test and train set
+attstrain,attstest, targettrain,targettest = train_test_split(atts, target, test_size=0.33)
+
+"""attstrain = atts[:129]
+attstest  = atts[129:]
+
+targettrain = df['etarget'][:129]
+targettest  = df['etarget'][129:]
+
+#attstrain, attstest = attstrain.reshape(1,-1), attstest.reshape """ 
+
+kn = KNeighborsClassifier()
+kn.fit(attstrain, targettrain)
+kn_pred = kn.score(attstest, targettest)
+print(f'KN predicts {kn_pred}% correctly.')
+
+mlp = MLPClassifier()
+mlp.fit(attstrain, targettrain)
+mlp_pred = mlp.score(attstest, targettest)
+print(f'MLP predicts {mlp_pred}% correctly.')
+
+dt = DecisionTreeClassifier()
+dt.fit(attstrain, targettrain)
+dt_pred = dt.score(attstest, targettest)
+print(f'Decision Tree predicts {dt_pred}% correctly.')
+
+"""def stress_cleaner(df):
+    df["Stress_c"] = df["Stress"]
+    
+    for i in df["Stress_c"]:
+        if i.isdigit():
+            if float(i) > 100 :
+                df["Stress_c"] = df["Stress_c"].replace(i,'100')
+        else:
+            df["Stress_c"] = df["Stress_c"].replace(i,'100')
+
+
+    df["Stress_c"] = pd.to_numeric(df["Stress_c"], errors='coerce', downcast='integer')
+        
+    return df"""
